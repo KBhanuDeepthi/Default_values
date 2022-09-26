94454dd473fd3e158134304f29186b4d14260c1c
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-09-26 20:50:05 +0530

    Added: Default values logic

---
+++ Default_values.java
@@ -0,0 +1,22 @@
+class Default_values {
+	static boolean val1;
+	static double val2;
+	static float val3;
+	static int val4;
+	static long val5;
+	static char val6;
+	static short val7;
+	static byte val8;
+	public static void main(String[] args) {
+		System.out.println("Default values of primitive datatypes are:");
+		System.out.println("boolean default value =" +val1);
+		System.out.println("double default value =" +val2);
+		System.out.println("float default value =" +val3);
+		System.out.println("int default value =" +val4);
+		System.out.println("long default value =" +val5);
+		System.out.println("char default value =" +val6);
+		System.out.println("short default value =" +val7);
+		System.out.println("byte default value =" +val8);
+	}
+
+}
